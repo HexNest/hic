5671a65bc9bcaed36e12d038f377b33bd64fe4bf
patrickfeltes <pj6444@gmail.com>
2018-10-18 18:15:09 -0500

    Add comments to code

--- hic.py
+++ hic.py
@@ -1,11 +1,13 @@
+import matplotlib.pyplot as plt
+
 def hic(interval_length, calibration = 4):
-	f = open('raw.txt')
-	lines = f.readlines()
-	f.close()
+	with open("raw.txt") as f:
+		lines = f.readlines()
 
 	# get rid of bad data
 	lines = lines[100:-100]
 
+	# each data point comes in the following form: time_stamp_ms, acceleration
 	time_stamps = list(map(lambda x : float(x.split()[0]), lines))
 	accelerations = list(map(lambda x : float(x.split()[1]), lines))
 	
@@ -16,34 +18,40 @@ def hic(interval_length, calibration = 4):
 	max_hic_start_index = -1
 	max_hic_end_index = -1
 
-	for start_index in range(len(time_stamps) - interval_length):
+	for start_index in range(len(time_stamps)):
 		start_timestamp = time_stamps[start_index]
 		end_index = start_index + 1
 
+		# move the end index forward until we reach the desired interval length
 		while time_stamps[end_index] - start_timestamp < interval_length:
 			end_index += 1
 
+			# if the interval length goes off the end, then go back 1 and stop adding to end_index
+			if end_index >= len(time_stamps):
+				end_index -= 1
+				break
+
 		sum_of_acceleration = sum(accelerations[start_index : end_index + 1])
 		avg_acceleration = sum_of_acceleration / float(end_index - start_index + 1)
+		# multiply by 0.001 to make convert from milliseconds to seconds
 		time_elapsed = (time_stamps[end_index] - time_stamps[start_index]) * 0.001
 
 		hic_value = time_elapsed * (avg_acceleration ** 2.5)
 
+		# if the current hic_value > max_hic, replace it
 		if hic_value > max_hic:
 			max_hic = hic_value
 			max_hic_start_index = start_index
 			max_hic_end_index = end_index
 
 	print("Max HIC: ", max_hic)
-	print("Start:", time_stamps[max_hic_start_index])
-	print("End:", time_stamps[max_hic_end_index])
+	print("Start Timestamp(ms):", time_stamps[max_hic_start_index])
+	print("End Timestamp(ms):", time_stamps[max_hic_end_index])
 	print(accelerations[max_hic_start_index : max_hic_end_index + 1])
-
-	import matplotlib.pyplot as plt
 
 	plt.plot(time_stamps, accelerations)
 	plt.show()
 
 	return max_hic
 
-hic(15)
+hic(interval_length = 15, calibration = 4)
